36c215eea0a66c998310cd02335b70c52a3db96b
Ryan Crowder <crowderyan@gmail.com>
2021-07-29 15:39:52 -0400

    get all route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,2 +1,21 @@
 const router = require("express").Router();
 const { Tag, Product, ProductTag } = require("../../models");
+
+router.get("/", (req, res) => {
+  // find all tags
+  // be sure to include its associated Product data
+  Tag.findAll({
+    attributes: ["id", "tag_name"],
+    include: [
+      {
+        model: Product,
+        attributes: ["id", "product_name", "price", "stock", "category_id"],
+      },
+    ],
+  })
+    .then((dbTagData) => res.json(dbTagData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
