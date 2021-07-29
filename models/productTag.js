00e4bad4b786430cacff937e823ca36a4cc26b87
Ryan Crowder <crowderyan@gmail.com>
2021-07-29 15:20:29 -0400

    built model

--- models/productTag.js
+++ models/productTag.js
@@ -5,3 +5,38 @@ const sequelize = require("../config/connection");
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class ProductTag extends Model {}
+
+ProductTag.init(
+  {
+    // define an id column
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    // define product_id column
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "product",
+        key: "id",
+      },
+    },
+    // define tag_id column
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "tag",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "product_tag",
+  }
+);
