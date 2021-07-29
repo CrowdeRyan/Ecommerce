04f2504b7b9d17dc0462be78f4ec331c05a322b4
Ryan Crowder <crowderyan@gmail.com>
2021-07-29 15:16:11 -0400

    added notes

--- models/category.js
+++ models/category.js
@@ -1,7 +1,9 @@
+// import important parts of sequelize library
 const { Model, DataTypes } = require("sequelize");
+// import our database connection from config.js
+const sequelize = require("../config/connection");
 
-const sequelize = require("../config/connection.js");
-
+// Initialize Category model (table) by extending off Sequelize's Model class
 class Category extends Model {}
 
 Category.init(
