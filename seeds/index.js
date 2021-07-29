2a6343db3fd6d2ce557421bea3295ecc3fde3bd0
Ryan Crowder <crowderyan@gmail.com>
2021-07-29 15:51:51 -0400

    call seeds

---
+++ seeds/index.js
@@ -0,0 +1,4 @@
+const seedCategories = require("./category-seeds");
+const seedProducts = require("./product-seeds");
+const seedTags = require("./tag-seeds");
+const seedProductTags = require("./product-tag-seeds");
