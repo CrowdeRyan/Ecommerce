70f33f246fa65ee1164e5d5bec4b7a8edb128b4e
Ryan Crowder <crowderyan@gmail.com>
2021-07-29 15:14:03 -0400

    built category model

--- models/category.js
+++ models/category.js
@@ -3,3 +3,27 @@ const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection.js");
 
 class Category extends Model {}
+
+Category.init(
+  {
+    // define an id column
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    // define category_name column
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "category",
+  }
+);
