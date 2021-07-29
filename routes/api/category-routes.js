2542086fbed1b70af7917525fc2bbdd871c7f01b
Ryan Crowder <crowderyan@gmail.com>
2021-07-29 15:29:27 -0400

    get all route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,2 +1,21 @@
 const router = require("express").Router();
 const { Category, Product } = require("../../models");
+
+router.get("/", (req, res) => {
+  // find all categories
+  // be sure to include its associated Products
+  Category.findAll({
+    attributes: ["id", "category_name"],
+    include: [
+      {
+        model: Product,
+        attributes: ["id", "product_name", "price", "stock", "category_id"],
+      },
+    ],
+  })
+    .then((dbCategoryData) => res.json(dbCategoryData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
