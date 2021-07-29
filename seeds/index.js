82f99aab87ccadbaca6c1ab3aaaa17befea7df06
Ryan Crowder <crowderyan@gmail.com>
2021-07-29 15:53:20 -0400

    async db

--- seeds/index.js
+++ seeds/index.js
@@ -4,3 +4,10 @@ const seedTags = require("./tag-seeds");
 const seedProductTags = require("./product-tag-seeds");
 
 const sequelize = require("../config/connection");
+
+const seedAll = async () => {
+  await sequelize.sync({ force: true });
+  console.log("\n----- DATABASE SYNCED -----\n");
+
+  process.exit(0);
+};
