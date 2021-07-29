3e1ffd00074a51afe85d94fb491ad9333cca72fb
Ryan Crowder <crowderyan@gmail.com>
2021-07-29 15:37:17 -0400

    delete route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -122,3 +122,23 @@ router.put("/:id", (req, res) => {
       res.status(400).json(err);
     });
 });
+
+router.delete("/:id", (req, res) => {
+  // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbProductData) => {
+      if (!dbProductData) {
+        res.status(404).json({ message: "No product found with this id" });
+        return;
+      }
+      res.json(dbProductData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
