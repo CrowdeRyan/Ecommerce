e9a01cc1e99227991ea3fc184a64295af8a28566
Ryan Crowder <crowderyan@gmail.com>
2021-07-29 15:59:21 -0400

    activate server

--- server.js
+++ server.js
@@ -7,3 +7,13 @@ const sequelize = require("./config/connection");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+// sync sequelize models to the database, then turn on the server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
+});
