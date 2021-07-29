8dec7551e0b4a9a9031d678fd63f115f3d0ae709
Ryan Crowder <crowderyan@gmail.com>
2021-07-29 15:18:17 -0400

    built model

--- models/product.js
+++ models/product.js
@@ -5,3 +5,53 @@ const sequelize = require("../config/connection");
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
+
+// set up fields and rules for Product model
+Product.init(
+  {
+    // define an id column
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    // define product_name column
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    // define price column
+    price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
+    },
+    // define stock column
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      },
+    },
+    // define category_id column
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "category",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "product",
+  }
+);
