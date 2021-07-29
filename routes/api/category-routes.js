0d0f562581808a2d84b438e82b21ccb5c130b659
Ryan Crowder <crowderyan@gmail.com>
2021-07-29 15:31:00 -0400

    delete route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -79,3 +79,23 @@ router.put("/:id", (req, res) => {
       res.status(500).json(err);
     });
 });
+
+router.delete("/:id", (req, res) => {
+  // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbCategoryData) => {
+      if (!dbCategoryData) {
+        res.status(404).json({ message: "No category found with this id" });
+        return;
+      }
+      res.json(dbCategoryData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
