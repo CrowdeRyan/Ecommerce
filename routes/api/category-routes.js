093d825b71a5256906d27a6e4f285fe22ef81612
Ryan Crowder <crowderyan@gmail.com>
2021-07-29 15:29:47 -0400

    get specific route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -19,3 +19,31 @@ router.get("/", (req, res) => {
       res.status(500).json(err);
     });
 });
+
+router.get("/:id", (req, res) => {
+  // find one category by its `id` value
+  // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "category_name"],
+    include: [
+      {
+        model: Product,
+        attributes: ["id", "product_name", "price", "stock", "category_id"],
+      },
+    ],
+  })
+    .then((dbCategoryData) => {
+      if (!dbCategoryData) {
+        res.status(404).json({ message: "No category found with this id" });
+        return;
+      }
+      res.json(dbCategoryData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
