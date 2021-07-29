9a12ce83e417d82aa5efe2596a19f25ca989951f
Ryan Crowder <crowderyan@gmail.com>
2021-07-29 15:58:27 -0400

    activate port

--- server.js
+++ server.js
@@ -4,3 +4,6 @@ const routes = require("./routes");
 
 // import sequelize connection
 const sequelize = require("./config/connection");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
