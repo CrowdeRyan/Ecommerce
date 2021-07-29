15c3b75d0ba56ed4581b61cc198669d61375ad19
Ryan Crowder <crowderyan@gmail.com>
2021-07-29 15:30:22 -0400

    general post route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -47,3 +47,15 @@ router.get("/:id", (req, res) => {
       res.status(500).json(err);
     });
 });
+
+router.post("/", (req, res) => {
+  // create a new category
+  Category.create({
+    category_name: req.body.category_name,
+  })
+    .then((dbCategoryData) => res.json(dbCategoryData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
