b4b7ee62c23fb317398970fd2dc2b6d52a5882a0
Ryan Crowder <crowderyan@gmail.com>
2021-07-29 15:30:39 -0400

    specific post route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -59,3 +59,23 @@ router.post("/", (req, res) => {
       res.status(500).json(err);
     });
 });
+
+router.put("/:id", (req, res) => {
+  // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbCategoryData) => {
+      if (!dbCategoryData[0]) {
+        res.status(404).json({ message: "No Category found with this id" });
+        return;
+      }
+      res.json(dbCategoryData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
