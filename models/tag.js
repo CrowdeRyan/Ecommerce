4d1a7b885f4cd73496683b387cfd3c24eb874f9c
Ryan Crowder <crowderyan@gmail.com>
2021-07-29 15:22:34 -0400

    import variables

---
+++ models/tag.js
@@ -0,0 +1,7 @@
+// import important parts of sequelize library
+const { Model, DataTypes } = require("sequelize");
+// import our database connection from config.js
+const sequelize = require("../config/connection");
+
+// Initialize Tag model (table) by extending off Sequelize's Model class
+class Tag extends Model {}
