29db4ddf9a139b2a15689626462fcf9fde3fdd51
Ryan Crowder <crowderyan@gmail.com>
2021-07-29 15:52:24 -0400

    call connection

--- seeds/index.js
+++ seeds/index.js
@@ -2,3 +2,5 @@ const seedCategories = require("./category-seeds");
 const seedProducts = require("./product-seeds");
 const seedTags = require("./tag-seeds");
 const seedProductTags = require("./product-tag-seeds");
+
+const sequelize = require("../config/connection");
