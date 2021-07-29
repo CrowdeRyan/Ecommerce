05344e4999005b4b337ef35e3b3664c7cd6c712f
Ryan Crowder <crowderyan@gmail.com>
2021-07-29 15:35:09 -0400

    get all route fixed

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,2 +1,26 @@
 const router = require("express").Router();
 const { Product, Category, Tag, ProductTag } = require("../../models");
+
+// get all products
+router.get("/", (req, res) => {
+  // find all products
+  // be sure to include its associated Category and Tag data
+  Product.findAll({
+    attributes: ["id", "product_name", "price", "stock", "category_id"],
+    include: [
+      {
+        model: Category,
+        attributes: ["id", "category_name"],
+      },
+      {
+        model: Tag,
+        attributes: ["id", "tag_name"],
+      },
+    ],
+  })
+    .then((dbProductData) => res.json(dbProductData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
