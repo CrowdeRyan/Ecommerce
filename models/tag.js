88411787f4928da18f5f7ec083923ce72905310a
Ryan Crowder <crowderyan@gmail.com>
2021-07-29 15:23:00 -0400

    built model

--- models/tag.js
+++ models/tag.js
@@ -5,3 +5,26 @@ const sequelize = require("../config/connection");
 
 // Initialize Tag model (table) by extending off Sequelize's Model class
 class Tag extends Model {}
+
+Tag.init(
+  {
+    // define an id column
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    // define tag_name column
+    tag_name: {
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "tag",
+  }
+);
