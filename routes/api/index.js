8cd1582db2e0c707a12cda1357016d7745595360
Ryan Crowder <crowderyan@gmail.com>
2021-07-29 15:43:50 -0400

    export tag

--- routes/api/index.js
+++ routes/api/index.js
@@ -6,3 +6,5 @@ const tagRoutes = require("./tag-routes");
 router.use("/categories", categoryRoutes);
 router.use("/products", productRoutes);
 router.use("/tags", tagRoutes);
+
+module.exports = router;
