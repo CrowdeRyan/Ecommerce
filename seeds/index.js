f7fac81c8737dea1fb86d021e724ea17f13ae637
Ryan Crowder <crowderyan@gmail.com>
2021-07-29 15:54:22 -0400

    await products

--- seeds/index.js
+++ seeds/index.js
@@ -8,9 +8,11 @@ const sequelize = require("../config/connection");
 const seedAll = async () => {
   await sequelize.sync({ force: true });
   console.log("\n----- DATABASE SYNCED -----\n");
-
   await seedCategories();
   console.log("\n----- CATEGORIES SEEDED -----\n");
+
+  await seedProducts();
+  console.log("\n----- PRODUCTS SEEDED -----\n");
 
   process.exit(0);
 };
