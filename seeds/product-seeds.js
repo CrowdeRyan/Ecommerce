5520aab3b18b1dde2fdc27d7a2c419dc4cbdc514
Ryan Crowder <crowderyan@gmail.com>
2021-07-31 23:01:18 -0400

    altered products

--- seeds/product-seeds.js
+++ seeds/product-seeds.js
@@ -2,31 +2,31 @@ const { Product } = require("../models");
 
 const productData = [
   {
-    product_name: "Plain T-Shirt",
+    product_name: "T-Shirt",
     price: 14.99,
     stock: 14,
     category_id: 1,
   },
   {
-    product_name: "Running Sneakers",
+    product_name: "Air Jordans",
     price: 90.0,
     stock: 25,
     category_id: 5,
   },
   {
-    product_name: "Branded Baseball Hat",
+    product_name: "Avengers DVD",
     price: 22.99,
     stock: 12,
     category_id: 4,
   },
   {
-    product_name: "Top 40 Music Compilation Vinyl Record",
+    product_name: "Now That's What I Call Music",
     price: 12.99,
     stock: 50,
     category_id: 3,
   },
   {
-    product_name: "Cargo Shorts",
+    product_name: "Cargo Khakis",
     price: 29.99,
     stock: 22,
     category_id: 2,
